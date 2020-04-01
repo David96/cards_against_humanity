7f49d673122d3e145632c6aaac3cdf7db2b45cea
David96 <david@hameipe.de>
2020-04-01 14:14:24 +0200

    Add backend implementation

--- cah.py
+++ cah.py
@@ -15,43 +15,76 @@ class Player:
 
 class CAH:
 
-    def __init__(self):
-        self.blacks = []
-        self.whites = []
-        self.white_stack = []
-        self.black_stack = []
+    def __init__(self, blacks, whites):
+        self.blacks = blacks
+        self.whites = whites
+        self.white_stack = list(whites)
+        self.black_stack = list(blacks)
         self.table = defaultdict(list)
         self.players = {}
         self.current = None
+        self.game_started = False
+        self.current_czar = 0
 
-    def select_card(self, selecting_player, selected_player):
+    def start_game(self):
+        self.game_started = True
+        for player in self.players.values():
+            player.score = 0
+        self.new_round()
+
+    def add_player(self, name):
+        self.players[name] = Player(name)
+
+    def get_owner_of_cards(self, selected_cards):
+        found = False
+        for name, cards in self.table.items():
+            print(set(cards))
+            print(set(selected_cards))
+            if set(cards) == set(selected_cards):
+                found = True
+                break
+        if not found:
+            raise 'Selected cards don\'t exist!'
+        return name
+
+    def select_cards(self, selecting_player, selected_cards):
         if not self.players[selecting_player].cardczar:
             raise 'Must be Card Czar to select a card!'
         if not self.check_round_finished():
             raise 'Round has not yet finished!'
-        self.players[selected_player].score += 1
+        name = self.get_owner_of_cards(selected_cards)
+        self.players[name].score += 1
         self.new_round()
+
+    def give_cards_to(self, player):
+        if not player.cardczar:
+            if len(self.white_stack) < CARD_COUNT - len(player.hand):
+                self.white_stack = list(self.whites)
+            sample = random.sample(self.white_stack, CARD_COUNT - len(player.hand))
+            for card in sample:
+                self.white_stack.remove(card)
+            player.hand += sample
 
     def give_cards(self):
         for player in self.players.values():
-            if len(self.white_stack) < CARD_COUNT - len(player.hand):
-                self.white_stack = list(self.whites)
-            player.hand += random.sample(self.white_stack, CARD_COUNT - len(player.hand))
-            for card in player.hand:
-                self.white_stack.remove(card)
+            self.give_cards_to(player)
 
-    def play_card(self, playername, *cards):
+    def play_cards(self, playername, cards):
         player = self.players[playername]
         if player.cardczar:
             raise 'Card Czar can\'t play a card!'
         if player.cards_played + len(cards) > self.blanks():
             raise 'Tried to play too many cards!'
         for card in cards:
-            self.table[player.name].append(player.hand[card])
+            if card not in player.hand:
+                raise 'Can\'t play a card you don\'t have!'
+            self.table[player.name].append(card)
             player.cards_played += 1
-            del player.hand[card]
+            player.hand.remove(card)
 
     def check_round_finished(self):
+        if not self.game_started:
+            return False
         for player in self.players.values():
             if not player.cardczar and player.cards_played != self.blanks():
                 return False
@@ -60,23 +93,29 @@ class CAH:
     def new_round(self):
         for player in self.players.values():
             player.cards_played = 0
+            player.cardczar = False
+        sorted_players = sorted(self.players.values(), key=lambda p1: p1.name)
+        self.current_czar = (self.current_czar + 1) % len(sorted_players)
+        sorted_players[self.current_czar].cardczar = True
         self.give_cards()
         if len(self.black_stack) < 1:
             self.black_stack = list(self.blacks)
-        self.current = random.sample(self.black_stack, 1)
+        self.current = random.sample(self.black_stack, 1)[0]
         self.black_stack.remove(self.current)
 
     def blanks(self):
-        return self.current.count('_')
+        return self.current.count('_') if self.current else 0
 
     def get_player_state(self, playername):
         player = self.players[playername]
         round_finished = self.check_round_finished()
-        cards_played = [l if name == playername else len(l) * [''] for name, l in self.table]
+        cards_played = [l if name == playername else len(l) * ['']
+                        for name, l in self.table.items()]
         return {
-            'table': self.table.values() if round_finished else cards_played,
-            'round_finished': self.check_round_finished(),
+            'table': list(self.table.values()) if round_finished else cards_played,
+            'round_finished': round_finished,
             'hand': player.hand,
             'current': self.current,
-            'blanks': self.blanks()
+            'blanks': self.blanks(),
+            'game_started': self.game_started,
         }
---
+++ main.py
@@ -0,0 +1,126 @@
+import asyncio
+import json
+import random
+import sys
+import traceback
+import websockets
+
+from cah import CAH
+
+with open('blacks.txt') as f:
+    BLACKS = [line.strip() for line in f.readlines()]
+with open('whites.txt') as f:
+    WHITES = [line.strip() for line in f.readlines()]
+
+GAME = CAH(BLACKS, WHITES)
+USERS = {}
+GAMEMASTERS = []
+
+def state_event(playername):
+    print(GAME.get_player_state(playername))
+    return json.dumps({
+        'type': 'state',
+        'state': GAME.get_player_state(playername)
+    })
+
+def user_event(_):
+    return json.dumps(
+        {
+            'type': 'user',
+            'players': [{'name': player.name, 'score': player.score, 'cardczar': player.cardczar}
+                        for player in GAME.players.values()]
+        })
+
+EVENTS = {
+    'user': user_event,
+    'state': state_event,
+}
+
+async def notify_users(msg_type):
+    for name, socket in USERS.items():
+        message = EVENTS[msg_type](name)
+        await socket.send(message)
+
+async def send_message(message):
+    if USERS:
+        msg = json.dumps({'type':'message', 'msg': message})
+        await asyncio.wait([socket.send(msg) for socket in USERS.values()])
+
+async def send_error(socket, message, error_type=''):
+    await socket.send(json.dumps({'type': 'error', 'error': error_type, 'msg': message}))
+
+async def add_user(websocket, name):
+    if name in GAME.players:
+        return None
+    GAME.add_player(name)
+    USERS[name] = websocket
+    await notify_users("user")
+    await send_message('%s joined the game!' % name)
+    return name
+
+async def remove_user(user):
+    del GAME.players[user]
+    del USERS[user]
+    await notify_users('user')
+    await send_message('%s left the game.' % user)
+
+async def select_cards(user, data):
+    GAME.select_cards(user, data['cards'])
+    owner = GAME.get_owner_of_cards(data['cards'])
+    await notify_users('state')
+    await notify_users('user')
+    await send_message('%s selected %s, point to %s' % (user, data['cards'], owner))
+
+async def play_cards(user, data):
+    GAME.play_cards(user, data['cards'])
+    await notify_users('state')
+
+async def start_game(user, data):
+    GAME.start_game()
+    await notify_users('state')
+    await notify_users('user')
+    await send_message('Game started by %s.' % user)
+
+ACTIONS = {
+    'select_cards': select_cards,
+    'play_cards': play_cards,
+    'start_game': start_game,
+}
+
+async def serve(websocket, path):
+    try:
+        user = None
+        async for message in websocket:
+            data = json.loads(message)
+            if 'action' not in data or data['action'] != 'set_name':
+                await send_error(websocket,
+                                 'First message must be a set_name action!', 'no_set_name')
+                await websocket.close()
+                return
+            user = await add_user(websocket, data['name'])
+            if user:
+                break
+            await send_error(websocket, 'Name is already taken!', 'name_taken')
+
+        await websocket.send(state_event(user))
+        async for message in websocket:
+            data = json.loads(message)
+            if data['action'] in ACTIONS:
+                try:
+                    await ACTIONS[data['action']](user, data)
+                except Exception as e:
+                    await send_error(websocket, str(e))
+                    traceback.print_exc()
+            else:
+                await send_error(websocket, '%s is not a valid action!' % data['action'],
+                                 'invalid_action')
+
+    # TODO: proper error handling?!
+    finally:
+        if user:
+            await remove_user(user)
+
+start_server = websockets.serve(serve, "localhost", 6789)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
